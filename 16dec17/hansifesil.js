89277f734f44b82db08bf62e869cfc5dd979e42c
Emil Ismayilzada <ismailzadeh2001@yandex.com>
2017-12-16 10:49:53 +0400

    Created hansifesil.js

---
+++ 16dec17/hansifesil.js
@@ -0,0 +1,13 @@
+var m = Number(prompt('Ayın daxil edin. (1-12)'));
+
+if(m==12||m==1||m==2){    
+    console.log('Daxil etdiyiniz ay qış ayıdır.');
+} else if(m==3||m==4||m==5) {
+    console.log('Daxil etdiyiniz ay yaz ayıdır.');
+} else if(m==6||m==7||m==8){
+    console.log('Daxil etdiyiniz ay yay ayıdır.');
+} else if(m==9||m==10||m==11){
+    console.log('Daxil etdiyiniz ay payız ayıdır.');
+} else{
+    console.log('Düzgün ay daxil edin.');
+}

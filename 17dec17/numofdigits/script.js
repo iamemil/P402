db7194a2abc976707570b90269526c0957ec0e3b
Emil Ismayilzada <ismailzadeh2001@yandex.com>
2017-12-17 12:28:53 +0400

    edited script.js

--- 17dec17/numofdigits/script.js
+++ 17dec17/numofdigits/script.js
@@ -2,11 +2,11 @@ var n = Number(prompt('Ədədi daxil edin'));
 var hedd = 1;
 var digit = 0;
 
-if(n>0){
- while(n >= hedd){
-     digit++;
-     hedd = hedd * 10;
- } console.log('Bu ədəd '+ digit + ' həddlidir.');
+if (n > 0) {
+    while (n >= hedd) {
+        digit++;
+        hedd = hedd * 10;
+    } console.log('Bu ədəd ' + digit + ' həddlidir.');
 } else {
-console.log('Müsbət ədəd daxil edin');
+    console.log('Müsbət ədəd daxil edin');
 }

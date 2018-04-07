ed1c087d7ecac93779e2f24eea8d686099567127
Emil Ismayilzada <ismailzadeh2001@yandex.com>
2018-04-07 12:26:20 +0400

    third

--- 7Apr18/7Apr18_first/7Apr18_first/Program.cs
+++ 7Apr18/7Apr18_first/7Apr18_first/Program.cs
@@ -24,27 +24,27 @@ namespace _7Apr18_first
 
             if (ExamResult > 0 && ExamResult <= 40)
             {
-                Console.WriteLine("Xal:" + ExamResult + " " + "Qiymet:G");
+                Console.WriteLine("Xal:" + ExamResult + " | " + "Qiymet:G");
             }
             else if (ExamResult > 40 && ExamResult <= 50)
             {
-                Console.WriteLine("Xal:" + ExamResult + " " + "Qiymet:F");
+                Console.WriteLine("Xal:" + ExamResult + " | " + "Qiymet:F");
             }
             else if(ExamResult > 50 && ExamResult <= 60)
             {
-                Console.WriteLine("Xal:" + ExamResult + " " + "Qiymet:E");
+                Console.WriteLine("Xal:" + ExamResult + " | " + "Qiymet:E");
             }
             else if (ExamResult > 60 && ExamResult <= 70)
             {
-                Console.WriteLine("Xal:" + ExamResult + " " + "Qiymet:D");
+                Console.WriteLine("Xal:" + ExamResult + " | " + "Qiymet:D");
             }
             else if (ExamResult > 70 && ExamResult <= 80)
             {
-                Console.WriteLine("Xal:" + ExamResult + " " + "Qiymet:C");
+                Console.WriteLine("Xal:" + ExamResult + " | " + "Qiymet:C");
             }
             else if (ExamResult > 80 && ExamResult <= 90)
             {
-                Console.WriteLine("Xal:" + ExamResult + " " + "Qiymet:B");
+                Console.WriteLine("Xal:" + ExamResult + " | " + "Qiymet:B");
             }
             else
             {

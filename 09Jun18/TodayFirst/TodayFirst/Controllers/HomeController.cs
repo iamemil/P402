c4aa93f59658d3cc3592d82d5263b43df24480b6
Emil Ismayilzada <ismailzadeh2001@yandex.com>
2018-06-09 12:47:03 +0400

    9 JUNE 2018 FILES

---
+++ 09Jun18/TodayFirst/TodayFirst/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TodayFirst.Models;
+
+namespace TodayFirst.Controllers
+{
+    public class HomeController : Controller
+    {
+        StudentContactEntities db = new StudentContactEntities();
+        public ActionResult Index()
+        {
+            ViewHome data = new ViewHome();
+
+            data.Students = db.Students.OrderByDescending(st => st.Id).ToList();
+            return View(data);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ 09Jun18/TodayFirst/TodayFirst/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TodayFirst.MvcApplication" Language="C#" %>
